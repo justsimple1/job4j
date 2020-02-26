51fe92a94fb0ab1a06e6fa98debc1e5512749d6b
justsimple1 <dla-raboti5@yandex.ru>
2020-02-26 22:14:13 +0300

    1. Конструктор по умолчанию.[#233855]

---
+++ chapter_001/src/main/java/ru/job4j/inheritance/Animal.java
@@ -0,0 +1,12 @@
+package ru.job4j.inheritance;
+
+public class Animal {
+    public Animal() {
+        super();
+        System.out.println("Animal");
+    }
+
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+    }
+}
