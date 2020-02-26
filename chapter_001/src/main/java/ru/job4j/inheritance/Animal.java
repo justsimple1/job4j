803b923f956d133e1999d1435028d5e30445cd48
justsimple1 <dla-raboti5@yandex.ru>
2020-02-26 22:29:30 +0300

    2. Перегрузка конструктора.[#233856]

--- chapter_001/src/main/java/ru/job4j/inheritance/Animal.java
+++ chapter_001/src/main/java/ru/job4j/inheritance/Animal.java
@@ -1,9 +1,15 @@
 package ru.job4j.inheritance;
 
 public class Animal {
+    private String name;
+
     public Animal() {
         super();
         System.out.println("Animal");
+    }
+
+    public Animal(String name) {
+        this.name = name;
     }
 
     public static void main(String[] args) {
