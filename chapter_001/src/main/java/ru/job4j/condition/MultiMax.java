3655d7815583d6a2a6d58a0ae3b0c478852631c4
justsimple1 <dla-raboti5@yandex.ru>
2020-02-26 20:32:25 +0300

    3. Перегрузить метод max для трех чисел[#233843]

--- chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -1,8 +1,13 @@
 package ru.job4j.condition;
 
 public class MultiMax {
+    public int max(int first, int second) {
+        return first > second ? first : second;
+    }
     public int max(int first, int second, int third) {
-        int result = first > second ? first : second;
-        return result > third ? result : third;
+        return max(max(first, second), third);
+    }
+    public int max(int first, int second, int third, int fourth) {
+        return max(max(first, second, third), fourth);
     }
 }
