6ace4be4df9f805c152c4f83d2cfd91c82eda42a
justsimple1 <dla-raboti5@yandex.ru>
2020-02-27 10:26:57 +0300

    2. Модель данных.[#233862]

---
+++ chapter_001/src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,13 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFio("Panin N. N.");
+        student.setGroup("intern");
+        student.setCreated(new Date());
+        System.out.println(student.getFio() + " entered in group - '" + student.getGroup() + "' : " + student.getCreated());
+    }
+}
