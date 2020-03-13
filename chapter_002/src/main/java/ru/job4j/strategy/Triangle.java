5c1082f37f3c62c6a24943a03a1549a1b6d3cfd0
justsimple1 <dla-raboti5@yandex.ru>
2020-03-13 08:46:02 +0300

    9.1 Рефакторинг теста @Before @After[#233810]

--- chapter_002/src/main/java/ru/job4j/strategy/Triangle.java
+++ chapter_002/src/main/java/ru/job4j/strategy/Triangle.java
@@ -2,14 +2,13 @@ package ru.job4j.strategy;
 
 import java.util.StringJoiner;
 
-public class Triangle implements Shape{
+public class Triangle implements Shape {
     public String draw() {
         StringJoiner pic = new StringJoiner(System.lineSeparator());
         pic.add("   +   ");
         pic.add("  +++  ");
         pic.add(" +++++ ");
         pic.add("+++++++");
-        pic.add(System.lineSeparator());
         return pic.toString();
     }
 }
