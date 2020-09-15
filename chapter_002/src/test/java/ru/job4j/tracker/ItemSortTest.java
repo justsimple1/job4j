121027fe26c8f2c23531b124f13be377871d3df7
justsimple1 <dla-raboti5@yandex.ru>
2020-09-15 19:37:48 +0300

    0. Сортировка[10096#233776] Update

--- chapter_002/src/test/java/ru/job4j/tracker/ItemSortTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/ItemSortTest.java
@@ -1,18 +1,15 @@
 package ru.job4j.tracker;
 
 import org.junit.Test;
-
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
-
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 
-public class ItemSort {
+public class ItemSortTest {
     @Test
     public void whenSortToHigh() {
         Item item = new Item();
@@ -26,7 +23,7 @@ public class ItemSort {
                 new Item("Find bugs"),
                 new Item("Load server")
         );
-        Collections.sort(items, new Item());
+        Collections.sort(items, new ru.job4j.collection.ItemSort());
         assertThat(item.compare(items.get(0), items2.get(0)), is(0));
         assertThat(item.compare(items.get(1), items2.get(1)), is(0));
         assertThat(item.compare(items.get(2), items2.get(2)), is(0));
@@ -45,10 +42,11 @@ public class ItemSort {
                 new Item("Find bugs"),
                 new Item("Create task")
         );
-        Comparator c = Collections.reverseOrder(new Item());
-        Collections.sort(items, c);
+
+        Collections.sort(items, new ru.job4j.collection.ItemSort().reversed());
         assertThat(item.compare(items.get(0), items2.get(0)), is(0));
         assertThat(item.compare(items.get(1), items2.get(1)), is(0));
         assertThat(item.compare(items.get(2), items2.get(2)), is(0));
     }
+
 }
