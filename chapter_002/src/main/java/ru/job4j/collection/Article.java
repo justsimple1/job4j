3d9e4f446846f449a29a4ee36de9d4cdd51a1cee
justsimple1 <dla-raboti5@yandex.ru>
2020-11-20 11:19:57 +0300

    2. Проверить две строчки на идентичность.
    1. Генератор текста

--- chapter_002/src/main/java/ru/job4j/collection/Article.java
+++ chapter_002/src/main/java/ru/job4j/collection/Article.java
@@ -1,20 +1,19 @@
 package ru.job4j.collection;
 
-import java.util.HashMap;
+import java.util.HashSet;
 
 public class Article {
     public static boolean generateBy(String origin, String line) {
         String[] text = origin.split(" ");
         String[] text2 = line.split(" ");
-        HashMap<String, Integer> map = new HashMap<>();
+        HashSet<String> arr = new HashSet<>();
         boolean result = true;
-        int i = 1;
-        for (String word: text) {
+        for (String word : text) {
             word = word.replaceAll("[^\\da-zA-Zа-яёА-ЯЁ ]", "");
-            map.put(word, i);
+            arr.add(word);
         }
-        for (String word: text2) {
-            if(map.getOrDefault(word, 0) == 0) {
+        for (String word : text2) {
+            if (!arr.contains(word)) {
                 result = false;
                 break;
             }
---
+++ chapter_002/src/main/java/ru/job4j/collection/FreezeStr.java
@@ -0,0 +1,33 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class FreezeStr {
+    public static boolean eq(String left, String right) {
+        char[] first = left.toCharArray();
+        char[] second = right.toCharArray();
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char symbol: first) {
+            if (map.containsKey(symbol)) {
+               int temp = map.get(symbol);
+                map.put(symbol, ++temp);
+            } else {
+                map.put(symbol, 1);
+            }
+        }
+        for (char symbol: second) {
+            if (map.containsKey(symbol)) {
+                if (map.get(symbol) > 1) {
+                    int temp = map.get(symbol);
+                    map.put(symbol, --temp);
+                } else {
+                    map.remove(symbol);
+                }
+            }
+            else {
+                break;
+            }
+        }
+        return map.size()==0;
+    }
+}
