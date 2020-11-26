169fc994bd09dd55857bc39bd9a5845e990d9e29
justsimple1 <dla-raboti5@yandex.ru>
2020-11-26 17:30:47 +0300

    4. Преобразование List в Map.[110226#233644]

--- chapter_004/src/main/java/ru/job4j/stream/School.java
+++ chapter_004/src/main/java/ru/job4j/stream/School.java
@@ -1,6 +1,7 @@
 package ru.job4j.stream;
 
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -8,5 +9,11 @@ public class School {
     public List<Student> collect(List<Student> students, Predicate<Student> predict) {
         List<Student> tenA = students.stream().filter(predict).collect(Collectors.toList());
         return tenA;
+    }
+
+    public Map<String, Student> collectToMap(List<Student> students) {
+        Map<String, Student> map = students.stream().collect(Collectors.toMap(
+                e -> e.getSurname(), e -> e, (s, a) -> s = a));
+        return map;
     }
 }
--- chapter_004/src/test/java/ru/job4j/stream/SchoolTest.java
+++ chapter_004/src/test/java/ru/job4j/stream/SchoolTest.java
@@ -4,7 +4,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 
 import static org.hamcrest.core.Is.is;
@@ -16,7 +18,10 @@ public class SchoolTest {
     @Before
     public void setUp() {
         students.add(new Student(10, "Surname1"));
+        students.add(new Student(10, "Surname1"));
         students.add(new Student(20, "Surname2"));
+        students.add(new Student(20, "Surname2"));
+        students.add(new Student(30, "Surname3"));
         students.add(new Student(30, "Surname3"));
         students.add(new Student(40, "Surname4"));
         students.add(new Student(50, "Surname5"));
@@ -56,9 +61,29 @@ public class SchoolTest {
         List<Student> rsl = sc.collect(students, pr);
         List<Student> expected = new ArrayList<>();
         expected.add(new Student(10, "Surname1"));
+        expected.add(new Student(10, "Surname1"));
+        expected.add(new Student(20, "Surname2"));
         expected.add(new Student(20, "Surname2"));
         expected.add(new Student(30, "Surname3"));
+        expected.add(new Student(30, "Surname3"));
         expected.add(new Student(40, "Surname4"));
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenListToMap() {
+        School sc = new School();
+        Map<String, Student> rsl = sc.collectToMap(students);
+        Map<String, Student> expected = new HashMap<>();
+        expected.put("Surname1", new Student(10, "Surname1"));
+        expected.put("Surname2", new Student(20, "Surname2"));
+        expected.put("Surname3", new Student(30, "Surname3"));
+        expected.put("Surname4", new Student(40, "Surname4"));
+        expected.put("Surname5", new Student(50, "Surname5"));
+        expected.put("Surname6", new Student(60, "Surname6"));
+        expected.put("Surname7", new Student(70, "Surname7"));
+        expected.put("Surname8", new Student(80, "Surname8"));
+        expected.put("Surname9", new Student(90, "Surname9"));
         assertThat(rsl, is(expected));
     }
 }
